52341052f6072b7b939ffc3060910679029d1f91
LEO <bbw1996@qq.com>
2024-02-04 21:30:46 +0800

    init

---
+++ commitlint.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['cz'],
+  rules: {
+    'subject-empty': [0, 'never'],
+    'type-empty': [0, 'never'],
+  },
+};
---
+++ index.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const os = require('os');
+
+const homedir = os.homedir();
+console.log(homedir);
